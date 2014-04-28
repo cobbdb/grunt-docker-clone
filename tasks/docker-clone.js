60913d5d8f4a7e0486dd6ba7553d620e85410537
cobbdb <cobbdb@gmail.com>
2014-04-28 16:07:07 -0700

    Use this.data instead of requiresConfig

--- tasks/docker-clone.js
+++ tasks/docker-clone.js
@@ -3,22 +3,27 @@
  */
 module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-exec');
-    grunt.registerMultiTask('docker-clone', 'Build docker to a special docs branch.', function () {
-        this.requiresConfig('inpath');
-        this.requiresConfig('branch');
+    grunt.registerMultiTask(
+        'docker-clone',
+        'Build docker to a special docs branch.',
+        function () {
+            if (!(this.data.src && this.data.branch)) {
+                grunt.log.error('Missing required inpath and branch settings from config.');
+            }
 
-        var path = require.resolve('grunt-docker-clone');
-        if (!path) {
-            grunt.log.error('Cannot find grunt-docker.sh!');
-            return false;
+            var path = require.resolve('grunt-docker-clone');
+            if (!path) {
+                grunt.log.error('Cannot find grunt-docker.sh!');
+                return false;
+            }
+
+            grunt.config.set('exec.docker-clone', {
+                cmd: path + '<% src %> <% branch %>'
+            });
+            grunt.task.run('grunt-exec:docker-clone');
+            this.requires('grunt-exec');
+
+            grunt.verbose.ok();
         }
-
-        grunt.config.set('exec.docker-clone', {
-            cmd: path + '<% src %> <% branch %>'
-        });
-        grunt.task.run('grunt-exec:docker-clone');
-        this.requires('grunt-exec');
-
-        grunt.verbose.ok();
-    });
+    );
 };
