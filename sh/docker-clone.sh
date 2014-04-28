b21b1fccef2d7e777ca54e7f82cd867cfa6bb8ba
cobbdb <cobbdb@gmail.com>
2014-04-28 16:56:41 -0700

    lkjlkj

--- sh/docker-clone.sh
+++ sh/docker-clone.sh
@@ -6,9 +6,7 @@ echo "Running docker-clone.sh:"
 
 # Make sure source directory exists.
 if [ ! "$1" ]; then
-    msg=$1
-    msg+=" was not found!"
-    echo "$msg" 1>&2
+    echo "$1 was not found!" 1>&2
     exit 1
 fi
 
@@ -20,7 +18,10 @@ if [ ! -d "docs-clone" ]; then
         echo "No origin set for this repository!" 1>&2
         exit 1
     else
-        git clone $repourl -b $2 docs-clone
+        git clone $repourl -b $2 docs-clone || {
+            echo "Could not find branch $2 on origin." 1>&2
+            exit 1
+        }
     fi
 elif [ ! -d "docs-clone/.git" ]; then
     echo "/docs-clone/ was found, but is not a git repository." 1>&2
@@ -37,7 +38,7 @@ node_modules/.bin/docker -i $1 -o docs-clone
 
 # Push the new docs.
 echo "- Updating docs branch."
-docsmsg = "docker-bld: "
+docsmsg="docker-bld: "
 docsmsg+=date
 git --git-dir=docs-clone/.git --work-tree=docs-clone add -A
 git --git-dir=docs-clone/.git --work-tree=docs-clone commit -m docsmsg
